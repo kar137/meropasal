bb8965a6965db5712c334aad17a125eb718f677b
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-07-12 09:56:55 +0545

    add kyc verification

--- meropasalapp/lib/screens/homepage.dart
+++ meropasalapp/lib/screens/homepage.dart
@@ -1,7 +1,3 @@
-// <<<<<<< HEAD:mobileapp/lib/screens/homepage.dart
-
-// =======
-// >>>>>>> dd53dafc3006b2740e18cb55abe14b2d9710381a:lib/screens/homepage.dart
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
